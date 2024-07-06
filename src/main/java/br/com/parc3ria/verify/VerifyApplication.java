35dc548606d5e123399e97aaf0a4d205bfa1ddd9
Rey Hall <reyh4ll@gmail.com>
2024-07-06 20:06:27 -0300

    VerifyApplication [Add] - Adicionado a class principal

--- src/main/java/br/com/parc3ria/verify/VerifyApplication.java
+++ src/main/java/br/com/parc3ria/verify/VerifyApplication.java
@@ -1,13 +1,20 @@
 package br.com.parc3ria.verify;
 
+import br.com.parc3ria.verify.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VerifyApplication {
+public class VerifyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerifyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.showMenu();
+	}
 }
---
+++ src/main/java/br/com/parc3ria/verify/principal/Principal.java
@@ -0,0 +1,7 @@
+package br.com.parc3ria.verify.principal;
+
+public class Principal {
+    public void showMenu(){
+        System.out.println("Hello World");
+    }
+}
