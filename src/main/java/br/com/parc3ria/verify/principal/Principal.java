a9d7a364a6187540292bd50a31b67e0c155ba26c
Rey Hall <reyh4ll@gmail.com>
2024-07-06 20:47:03 -0300

    VerifyApplication [Add] - Scanner adicionando + getValidationChoice

--- src/main/java/br/com/parc3ria/verify/principal/Principal.java
+++ src/main/java/br/com/parc3ria/verify/principal/Principal.java
@@ -5,14 +5,12 @@ import br.com.parc3ria.verify.service.RequestAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.constraints.NotNull;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Principal {
     private RequestAPI requestAPI = new RequestAPI();
     private Conversor conversor = new Conversor();
+    private Scanner scanner = new Scanner(System.in);
     private static final String URL_BASE_EMAIL = "http://emailvalidation.abstractapi.com/v1/?";
     private List<String> listaChoice = Arrays.asList("Email");
     private Map<String, String> apiKeys = new HashMap<>();
@@ -24,6 +22,16 @@ public class Principal {
     @Autowired
 
     public void showMenu(){
+        listaChoice.forEach(System.out::println);
+        System.out.print("Digite qual opção acima deseja escolher: ");
+        var choice = getValidationChoice(scanner.nextLine());
+    }
 
+    private String getValidationChoice(String choice){
+        return listaChoice.stream()
+                .map(String::toLowerCase)
+                .filter(f -> f.contains(choice.toLowerCase()))
+                .findFirst()
+                .orElseGet(() -> listaChoice.get((int) (Math.random() * listaChoice.size())));
     }
 }
