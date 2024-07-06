a42e244bf6e470980a961dadeb61e367a61bfda4
Rey Hall <reyh4ll@gmail.com>
2024-07-06 18:06:51 -0300

    First Commit

---
+++ src/test/java/br/com/parc3ria/verify/VerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.parc3ria.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
